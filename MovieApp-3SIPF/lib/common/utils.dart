e3c4c55e7ff12d93528544e384abc2d2bf4f4a06
lorenzo-nascimento <lorenzonascimento.dev@gmail.com>
2024-09-20 20:17:13 -0300

    Alterando a API key

--- MovieApp-3SIPF/lib/common/utils.dart
+++ MovieApp-3SIPF/lib/common/utils.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
 const kBackgoundColor = Color(0xff121012);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
-const apiKey = '5f9d4bd0608c0d8b303396a95e29c3a5';
+const apiKey = '3025e4b95a3c01e8ec46996f14f741bf';
 Color backgroundPrimary = Color.fromARGB(255, 171, 169, 180);
